2ec34af328a8de5836a7103614347d4edb481c52
박재중 <jjp910123@gmail.com>
2025-06-10 16:13:24 +0900

    라우팅 처리

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  publicDir: 'public',
   server: {
     host: true, // 또는 '0.0.0.0'
     port: 5173, // 생략 가능하지만 명시하면 명확해요
