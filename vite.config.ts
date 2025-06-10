fa1a480011eaec4c2257f5e8cd1f776f74e74bbd
박재중 <jjp910123@gmail.com>
2025-06-10 11:42:10 +0900

    🐛 fix: 잘못된 이미지 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     host: true, // 또는 '0.0.0.0'
     port: 5173, // 생략 가능하지만 명시하면 명확해요
     allowedHosts: ['cooking-prime-indicating-velocity.trycloudflare.com'],
-    proxy: {
-      '/api': {
-        target: 'https://operational-movers-convergence-starting.trycloudflare.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    }
   },
 });
